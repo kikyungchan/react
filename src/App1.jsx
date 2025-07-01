045f71db6f9605ec3fc27e9d4d3fca5c88a4f3fc
kikyungchan <kikyungchan@gmail.com>
2025-07-01 09:37:53 +0900

    리액트 코드 연습

---
+++ src/App1.jsx
@@ -0,0 +1,8 @@
+function App1() {
+  return (
+    <>
+      <p>두번 째 만드는 리액트 코드</p>
+    </>
+  );
+}
+export default App1;
---
+++ src/App2.jsx
@@ -0,0 +1,8 @@
+function App2() {
+  return (
+    <>
+      <h2>내가 만든 h2코드</h2>
+    </>
+  );
+}
+export default App2;
