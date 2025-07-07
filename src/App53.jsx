4628b70c5317f800f77715bab0258b35147541f3
kikyungchan <kikyungchan@gmail.com>
2025-07-07 09:44:39 +0900

    axios 요청 보내기

--- src/App53.jsx
+++ src/App53.jsx
@@ -9,8 +9,47 @@ function App53(props) {
     });
   }
 
+  function handleButton2Click() {
+    axios.request({
+      method: "get",
+      url: "/api/main28/sub1",
+    });
+  }
+
+  function handleButton3Click() {
+    axios.request({
+      method: "post",
+      url: "/api/main28/sub2",
+    });
+  }
+
+  function hamdleButton4Click() {
+    axios.request({
+      method: "get",
+      url: "/api/main28/sub3",
+    });
+  }
+
+  function handleButton5Click() {
+    axios.request({
+      method: "post",
+      url: "/api/main28/sub4",
+    });
+  }
+
   return (
     <div>
+      {/* 연습 : 버튼4 클릭시 get방식 /api/main28/sub3 으로 요청*/}
+      {/* 연습 : 버튼5 클릭시 post방식 /api/main28/sub4 으로 요청*/}
+      {/* 각각의 버튼 서버에서 요청받는 method*/}
+      <button onClick={handleButton5Click}>요청 보냄5</button>
+      <hr />
+      <button onClick={hamdleButton4Click}>요청 보냄4</button>
+      <hr />
+      <button onClick={handleButton3Click}>요청 보내기 3</button>
+      <hr />
+      <button onClick={handleButton2Click}>요청 보내기 2</button>
+      <hr />
       <button onClick={handleButton1Click}>요청 보내기 1</button>
     </div>
   );
